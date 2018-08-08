9f796b2adec725473adae9781925eeea24233794
king <2772455683@qq.com>
2018-08-08 15:28:38 +0800

    测试idea提交git

---
+++ src/main/java/com/mrw/king/KingApplication.java
@@ -0,0 +1,12 @@
+package com.mrw.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingApplication.class, args);
+    }
+}
